9e61b68df0e3538cae9d929c58a0f68889a76de0
dstlmike <dstl_mike1@hotmail.com>
2021-02-22 02:40:40 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,7 @@ class App extends PureComponent<{}> {
           <p className="Header__Title">GridFS - Upload and List Files</p>
 
           <div className="Menu">
-            <Link to='/'>
+            <Link to='/uploads/'>
               <img
                 src={Upload}
                 alt="upload"
@@ -34,8 +34,8 @@ class App extends PureComponent<{}> {
         <div className="Routes">
           <Switch>
             <Route exact path='/uploads/' component={UploadPage} />
-            <Route exact path='/list' component={ListPage} />
-            <Redirect from='/**' to='/uploads/' />
+            <Route exact path='/uploads/list' component={ListPage} />
+            <Redirect from='/**' to='/' />
           </Switch>
         </div>
       </div>
